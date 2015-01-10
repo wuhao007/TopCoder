e63261fd88dc6d788a6f3bbf0e666e82f9575190
Hao Wu <tohaowu@gmail.com>
2015-01-10 14:30:14 -0500

    I don't know why I just got 75 it should give some test result

---
+++ MatchMaker.py
@@ -0,0 +1,29 @@
+class MatchMaker:
+    def getBestMatches(self, members, currentUser, sf):
+        current_gender = ''
+        current_request = ''
+        current_answers = []
+        for member in members:
+            element = member.split()
+            if element[0] == currentUser:
+                current_gender = element[1]
+                current_request = element[2]
+                current_answers += element[3:]
+                break
+        ret = {}
+        for member in members:
+            element = member.split()
+            if element[0] != currentUser:
+                if current_request == element[1] and current_gender == element[2]:
+                    score = 0
+                    for i in range(len(current_answers)):
+                        if current_answers[i] == element[3 + i]:
+                            score += 1
+                    if score >= sf and score not in ret:
+                        ret[score] = element[0]
+        return ret.values()
+match_maker = MatchMaker()
+members = ("BETTY F M A A C C", "TOM M F A D C A", "SUE F M D D D D", "ELLEN F M A A C A", "JOE M F A A C A", "ED M F A D D A", "SALLY F M C D A B", "MARGE F M A A C C")
+print match_maker.getBestMatches(members, "BETTY", 2)
+print match_maker.getBestMatches(members, "JOE", 1)
+print match_maker.getBestMatches(members, "MARGE", 4)
